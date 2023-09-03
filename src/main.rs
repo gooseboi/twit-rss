8a193981bf4f0f32a8e576b1b551092f59c79356
Guzman Zugnoni <gooseiman@protonmail.com>
2023-09-03 16:47:23 -0300

    start getting posts

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ use std::env;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::time::{sleep, Duration};
 
+const MAX_LINKS_PER_FETCH: usize = 50;
+const AUTH_CACHE_FNAME: &str = "cached_auth";
+
+fn sleep_secs(n: usize) -> tokio::time::Sleep {
+    sleep(Duration::from_secs(n as u64))
+}
+
 async fn auth(c: &Client) -> Result<Vec<Cookie<'static>>, fantoccini::error::CmdError> {
     let username =
         env::var("TWITTER_USERNAME").expect("Could not load twitter username from environment!");
@@ -13,7 +20,7 @@ async fn auth(c: &Client) -> Result<Vec<Cookie<'static>>, fantoccini::error::Cmd
         env::var("TWITTER_PASSWORD").expect("Could not load twitter password from environment!");
 
     c.goto("https://twitter.com/").await?;
-    sleep(Duration::from_secs(5)).await;
+    sleep_secs(5).await;
     if c.source().await?.as_str().contains("This page is down") {
         panic!("Twitter is down");
     }
@@ -25,16 +32,16 @@ async fn auth(c: &Client) -> Result<Vec<Cookie<'static>>, fantoccini::error::Cmd
     .click()
     .await?;
     println!("Opened the sign in box");
-    sleep(Duration::from_secs(3)).await;
+    sleep_secs(3).await;
     c.find(Locator::XPath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")).await?.click().await?;
     println!("Clicked on the username box");
-    sleep(Duration::from_secs(3)).await;
+    sleep_secs(3).await;
     c.find(Locator::XPath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")).await?.send_keys(username.as_str()).await?;
     println!("Typed in the username box");
-    sleep(Duration::from_secs(1)).await;
+    sleep_secs(1).await;
     c.find(Locator::XPath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]")).await?.click().await?;
     println!("Clicked on the next button");
-    sleep(Duration::from_secs(5)).await;
+    sleep_secs(5).await;
 
     if c.source()
         .await?
@@ -44,20 +51,144 @@ async fn auth(c: &Client) -> Result<Vec<Cookie<'static>>, fantoccini::error::Cmd
         println!("Got the confirmation dialog");
         c.find(Locator::XPath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input")).await?.send_keys(username.as_str()).await?;
         println!("  Inputted the username");
-        sleep(Duration::from_secs(2)).await;
+        sleep_secs(2).await;
         c.find(Locator::XPath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div")).await?.click().await?;
         println!("  Clicked on the button");
-        sleep(Duration::from_secs(3)).await;
+        sleep_secs(3).await;
     }
 
     c.find(Locator::XPath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input")).await?.send_keys(password.as_str()).await?;
     println!("Typed in the pasword");
-    sleep(Duration::from_secs(3)).await;
+    sleep_secs(3).await;
     c.find(Locator::XPath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div")).await?.click().await?;
     println!("Clicked on the log in button");
-    sleep(Duration::from_secs(7)).await;
+    sleep_secs(7).await;
 
     c.get_all_cookies().await
+}
+
+async fn set_auth_cookie(c: &Client) -> Result<(), fantoccini::error::CmdError> {
+    println!("Loading auth");
+    let cached = tokio::fs::File::open(AUTH_CACHE_FNAME).await;
+    if let Ok(mut f) = cached {
+        println!("Found cached auth!");
+        let mut contents = vec![];
+        f.read_to_end(&mut contents).await.unwrap();
+        // For some reason, Clients can only add cookies with 'static, so
+        // this must be leaked
+        let s = String::from_utf8(contents).unwrap().leak();
+        let cookie = Cookie::parse(&*s).unwrap();
+        c.goto("https://twitter.com").await?;
+        c.add_cookie(cookie).await?;
+        c.refresh().await?;
+    } else {
+        println!("Reloading auth from site");
+        let cookies = auth(c).await?;
+
+        let cookie = cookies
+            .iter()
+            .filter(|c| c.name() == "auth_token")
+            .last()
+            .unwrap()
+            .clone();
+        let mut f = tokio::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(AUTH_CACHE_FNAME)
+            .await
+            .unwrap();
+        f.write_all(cookie.to_string().as_str().as_bytes())
+            .await
+            .unwrap();
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Post {
+    link: String,
+    date: chrono::DateTime<chrono::offset::Utc>,
+    text: String,
+    retweeter: Option<String>,
+}
+
+async fn get_recent_posts_for_user(
+    c: &Client,
+    user_id: &str,
+) -> Result<Vec<Post>, fantoccini::error::CmdError> {
+    c.goto(&format!("https://twitter.com/{user_id}")).await?;
+    sleep_secs(4).await;
+    let username = {
+        let doc = Html::parse_document(&c.source().await?);
+        let div_selector = &Selector::parse("div").unwrap();
+        doc.select(div_selector)
+            .filter(|e| {
+                e.value().attrs().any(|(k, v)| {
+                    k.eq_ignore_ascii_case("data-testid") && v.eq_ignore_ascii_case("UserName")
+                })
+            })
+            .map(|e| e.text().collect::<Vec<_>>().join(" "))
+            .next()
+            .unwrap()
+            .split(" @") // <username> @<user_id>
+            .next()
+            .unwrap()
+            .trim()
+            .to_owned()
+    };
+    println!("Downloading data for {username}");
+
+    let re = Regex::new(&format!("^/\\w+/status/\\d+$")).unwrap();
+    let anchor_selector = &Selector::parse("a").unwrap();
+    let article_selector = &Selector::parse("article").unwrap();
+    let user_status_format = &format!("/{user_id}");
+    let mut links = indexmap::IndexSet::new();
+
+    while links.len() < MAX_LINKS_PER_FETCH
+        || !links
+            .last()
+            .map(|l: &String| l.starts_with(user_status_format))
+            .unwrap_or(true)
+    {
+        c.execute("window.scrollBy(0,300);", vec![]).await?;
+        sleep_secs(1).await;
+
+        let s = c.source().await?;
+        let doc = Html::parse_document(&s);
+        let link_iter = doc
+            .select(article_selector)
+            .flat_map(|article| article.select(anchor_selector))
+            .filter_map(|e| e.value().attr("href"))
+            .filter(|l| re.is_match(l))
+            .map(|l| l.to_owned());
+
+        links.extend(link_iter);
+        println!("{}:{links:#?}", links.len());
+    }
+
+    let mut posts = vec![];
+    for i in 0..links.len() {
+        let link = links.get_index(i).unwrap();
+        if link.starts_with(user_status_format) {
+            let post = get_post(c, link).await?;
+            posts.push(post);
+            continue;
+        }
+        todo!()
+    }
+
+    Ok(posts)
+}
+
+async fn get_post(c: &Client, link: &str) -> Result<Post, fantoccini::error::CmdError> {
+    let full_link = &format!("https://twitter.com{link}");
+    c.goto(full_link).await?;
+    sleep_secs(3).await;
+    let doc = Html::parse_document(&c.source().await?);
+    let section_selector = &Selector::parse("section").unwrap();
+    println!("{}", doc.select(section_selector).count());
+
+    todo!()
 }
 
 #[tokio::main]
@@ -71,7 +202,6 @@ async fn main() -> Result<(), fantoccini::error::CmdError> {
             },
         }),
     );
-    println!("Starting with caps: {caps:?}");
 
     let c = ClientBuilder::rustls()
         .capabilities(caps)
@@ -79,74 +209,11 @@ async fn main() -> Result<(), fantoccini::error::CmdError> {
         .await
         .expect("failed to connect to WebDriver");
 
-    println!("Attempting to load cached_auth");
-    let cached = tokio::fs::File::open("cached_auth").await;
-    if let Ok(mut f) = cached {
-        println!("Found cached auth!");
-        let mut contents = vec![];
-        f.read_to_end(&mut contents).await.unwrap();
-        // For some reason, Clients can only add cookies with 'static, so
-        // this must be leaked
-        let s = String::from_utf8(contents).unwrap().leak();
-        let cookie = Cookie::parse(&*s).unwrap();
-        c.goto("https://twitter.com").await?;
-        c.add_cookie(cookie).await?;
-        c.refresh().await?;
-    } else {
-        println!("Reloading cached_auth");
-        let res = auth(&c).await;
-        if let Err(e) = res {
-            c.close().await?;
-            return Err(e);
-        }
+    set_auth_cookie(&c).await?;
 
-        let cookie = res
-            .unwrap()
-            .iter()
-            .filter(|c| c.name() == "auth_token")
-            .last()
-            .unwrap()
-            .clone();
-        let mut f = tokio::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open("cached_auth")
-            .await
-            .unwrap();
-        f.write_all(cookie.to_string().as_str().as_bytes())
-            .await
-            .unwrap();
-    }
-
-    let username = "jonhoo";
-    c.goto(&format!("https://twitter.com/{username}")).await?;
-    sleep(Duration::from_secs(4)).await;
-
-    let re = Regex::new(&format!("^/\\w+/status/\\d+$")).unwrap();
-    let anchor_selector = &Selector::parse("a").unwrap();
-    for _ in 0..100 {
-        c.execute("window.scrollBy(0,300);", vec![]).await?;
-        sleep(Duration::from_secs(2)).await;
-
-        let s = c.source().await?;
-        let doc = Html::parse_document(&s);
-        let els = doc
-            .select(&Selector::parse("article").unwrap())
-            .flat_map(|article| article.select(anchor_selector))
-            .filter_map(|e| e.value().attr("href").map(|l| (e, l)))
-            .filter(|(_, l)| re.is_match(l))
-            .collect::<Vec<_>>();
-
-        for (_, l) in els {
-            // FIXME: This may either a post or a retweet (not a repost)
-            // Posts could be further separate into normal posts,
-            // quote-retweets (not reposts) and comments
-            // However, the main distinction is for retweets,
-            // as these yield the url for the retweeted tweet, as they
-            // do not generate a new "tweet". It is also unclear how
-            // a date for a retwee could be extracted.
-            println!("{}", l);
-        }
+    let posts = get_recent_posts_for_user(&c, "jonhoo").await?;
+    for post in posts {
+        println!("{post:?}");
     }
 
     c.close().await
